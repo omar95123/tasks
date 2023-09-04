8fdd615a1faa4d401267881dbfac2d85b65635b1
Omar Taher <omartaher330@gmail.com>
2023-09-04 19:31:00 +0300

    Add files via upload

---
+++ task21.dart
@@ -0,0 +1,24 @@
+import 'dart:io';
+import 'dart:math';
+
+
+void main() {
+  int x = 100;
+  int numberOfGuess = 0;
+
+  do {
+    int randomNumber = Random().nextInt(x);
+    print("Number is: $randomNumber");
+    String? input = stdin.readLineSync();
+    numberOfGuess += 1;
+    if (input == 'high') {
+      x < randomNumber;
+    } else if (input == 'low') {
+      x > randomNumber;
+    }
+    else if (input == 'right') {
+      print('grate');
+      print(numberOfGuess);
+    }
+  } while (true);
+}
