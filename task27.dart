73857972adce063f975aa11fbebc69a7c0a5d169
Omar Taher <omartaher330@gmail.com>
2023-09-05 13:08:41 +0300

    Add files via upload

---
+++ task27.dart
@@ -0,0 +1,34 @@
+import 'dart:io';
+
+
+
+void main() {
+  var scientists ={'Albert Einstein':'04/1/1996','Benjamin Franklin':'20/8/2000','Ada Lovelace':'16/6/1833'};
+  print('Welcome to the birthday dictionary');
+  print('Who\'s birthday do you want to look up?');
+  print(scientists.keys);
+  String name = stdin.readLineSync()!;
+
+  print('${name}  birthday is ${scientists[name]}');
+  print('add  scientists');
+
+  String add = stdin.readLineSync()!;
+  do{
+
+    if (add == 'yes'){
+      print('name');
+      String newName = stdin.readLineSync()!;
+      print('birthday');
+      String birthday = stdin.readLineSync()!;
+      scientists[newName] = birthday;
+      print(scientists);print('add  scientists');
+      add = stdin.readLineSync()!;
+    }else{
+      print('ok');
+    }
+
+  }while(add =='yes');
+
+
+
+}
