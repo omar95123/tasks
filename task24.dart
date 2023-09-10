bf60d103743c1efc77eb2245beea5351251c39a2
Omar Taher <omartaher330@gmail.com>
2023-09-10 11:10:47 +0300

    Add files via upload

---
+++ task24.dart
@@ -0,0 +1,43 @@
+import 'dart:io';
+
+
+
+void main() {
+  String randomWord = "evaporate";
+  guessLetter(randomWord);
+}
+
+void guessLetter(String randomWord) {
+
+
+  List guess = (" " * randomWord.length).split("");
+
+
+
+  int i = 0;
+  do {
+    i = i+1;
+    stdout.write("guess a letter: ");
+    String input = stdin.readLineSync()!;
+    for (var i = 0; i < randomWord.length; i++) {
+      if (randomWord[i] == input) {
+        guess[i] = input;
+      }
+    }
+    print(guess);
+
+  }while (guess != randomWord);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
