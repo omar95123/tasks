583539f9e28a96294787c872fa84ffdd99141cf5
Omar Taher <omartaher330@gmail.com>
2023-09-10 11:53:16 +0300

    Update task24.dart

--- task24.dart
+++ task24.dart
@@ -1,43 +1,27 @@
 import 'dart:io';
 
 
-
 void main() {
-  String randomWord = "evaporate";
+   String randomWord = "evaporate";
   guessLetter(randomWord);
 }
 
 void guessLetter(String randomWord) {
-
-
   List guess = (" " * randomWord.length).split("");
-
-
-
   int i = 0;
   do {
     i = i+1;
     stdout.write("guess a letter: ");
     String input = stdin.readLineSync()!;
     for (var i = 0; i < randomWord.length; i++) {
-      if (randomWord[i] == input) {
+     if (randomWord[i] == input) {
         guess[i] = input;
+       if(guess.join('')==randomWord){
+         print('Guess right');
+       }
       }
     }
-    print(guess);
-
-  }while (guess != randomWord);
+    print(guess.join(''));
+    
+  }while (guess.join('') != randomWord);
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
