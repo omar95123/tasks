af086f5765b36f902a75aae1182ec7f698f6131b
Omar Taher <omartaher330@gmail.com>
2023-09-14 12:04:22 +0300

    Update task27.dart

--- task27.dart
+++ task27.dart
@@ -3,35 +3,13 @@ import 'dart:io';
 
 
 void main() {
-  var scientists ={'Albert Einstein':'04/1/1996','Benjamin Franklin':'20/8/2000','Ada Lovelace':'16/6/1833'};
+  Map<String,String> scientists ={'Albert Einstein':'04/1/1996','Benjamin Franklin':'20/8/2000','Ada Lovelace':'16/6/1833'};
   print('Welcome to the birthday dictionary');
   print('Who\'s birthday do you want to look up?');
   print(scientists.keys);
   String name = stdin.readLineSync()!;
   print('${name}  birthday is ${scientists[name]}');
-    print('other scientist');
-  
-  String otherScientist = stdin.readLineSync()!;
-
-
-  do{
-    if(otherScientist =='yes'){
-      print('Who\'s birthday do you want to look up?');
-      String name = stdin.readLineSync()!;
-
-      if(name ==scientists.keys){
-        print('${name}  birthday is ${scientists[name]}');
-      }else{
-        print('rong name');
-      }
-      print('other scientist');
-      otherScientist = stdin.readLineSync()!;
-    }else{
-      print('add  scientists');
-    }
-
-  }while(otherScientist =='yes');
-  
+  print(' add  scientists');
   String add = stdin.readLineSync()!;
   do{
 
